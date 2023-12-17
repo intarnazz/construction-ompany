c1a2c5d0d9e6e9cd9d81928cbf93c9d3ba6e1bbb
Od saleva <misha.vidiakin@gmail.com>
2023-12-18 00:37:53 +0300

    JS

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,18 @@
 function scrollTopEvent() {
   document.documentElement.scrollTop = 0;
 }
+
+async function handleResize() {
+  setTimeout(() => {
+    const imgBoxArr = document.querySelectorAll(".list-offers__info-wrapper");
+    const imgArr = document.querySelectorAll(".list-offers__info-bg");
+    let i = 0;
+    imgBoxArr.forEach((imgBox) => {
+      imgArr[i].style.height = `${imgBox.clientHeight}px`;
+      console.log(imgArr[i].clientHeight);
+      i++;
+    });
+  }, 600);
+}
+handleResize();
+window.addEventListener("resize", handleResize);
